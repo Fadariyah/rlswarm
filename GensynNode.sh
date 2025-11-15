19a08cd7184b4c8490193fe115ab8ed84b348212
Fadariyah <82722709+Fadariyah@users.noreply.github.com>
2025-11-15 08:46:15 +0700

    Update GensynNode.sh

--- GensynNode.sh
+++ GensynNode.sh
@@ -59,9 +59,7 @@ system_update_and_install() {
     sudo npm install -g localtunnel
     
     # Install Node.js 22.x
-    sudo apt-get update
-    curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-    sudo apt-get install -y nodejs
+    curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash - && sudo apt-get install -y nodejs
     
     # Check versions
     node -v
